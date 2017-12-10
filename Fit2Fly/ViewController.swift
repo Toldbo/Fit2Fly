0ba03b7f6ab043b56958e1e889b78da0741bbd70
Christina Toldbo <cto@nine.dk>
2017-12-10 17:31:59 +0100

    Initial Commit

---
+++ Fit2Fly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fit2Fly
+//
+//  Created by Christina Toldbo on 10/12/2017.
+//  Copyright © 2017 Christina Toldbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fit2FlyTests/Fit2FlyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fit2FlyTests.swift
+//  Fit2FlyTests
+//
+//  Created by Christina Toldbo on 10/12/2017.
+//  Copyright © 2017 Christina Toldbo. All rights reserved.
+//
+
+import XCTest
+@testable import Fit2Fly
+
+class Fit2FlyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
