64595dbd2a6c19aa07dd1490226e970d2a37219d
Christina Toldbo <christina.toldbo@gmail.com>
2018-05-16 20:57:33 +0200

    Instalation of pod and charts + extra view controller

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Fit2Fly' do
+    pod 'Charts'
+end
---
+++ Pods/Target Support Files/Pods-Fit2Fly/Pods-Fit2Fly-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fit2Fly : NSObject
+@end
+@implementation PodsDummy_Pods_Fit2Fly
+@end
